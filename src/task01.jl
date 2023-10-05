c73b7d843c16efdb98c1f5f0a2585f38b1e3b011
Katerina Rudenko <kdr.katerina@yandex.ru>
2023-10-05 20:57:14 +0300

    getdigits implemented

--- src/task01.jl
+++ src/task01.jl
@@ -1,7 +1,16 @@
 export getdigits, touppercase, firstnorm, secondnorm, infnorm, isleap, chesscolor
 
 function getdigits(a)
-    return 0
+    if a == 0
+        return [0]
+    end
+    B = Int[]  # Создаем пустой массив для хранения цифр
+    while a != 0
+        c = a % 10  # Получаем последнюю цифру числа a
+        push!(B, c)  # Добавляем цифру в массив
+        a = div(a, 10)  # Убираем последнюю цифру числа a
+    end
+    return reverse(B)  # Обратный порядок цифр в массиве для соответствия порядку в числе
 end
 
 function touppercase(str_)
