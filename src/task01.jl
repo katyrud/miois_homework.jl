88990b5168d190132c1666b5f9072e25baa505d5
Katerina Rudenko <kdr.katerina@yandex.ru>
2023-10-06 16:28:40 +0300

    implemented

--- src/task01.jl
+++ src/task01.jl
@@ -1,17 +1,17 @@
 export getdigits, touppercase, firstnorm, secondnorm, infnorm, isleap, chesscolor
 
 function getdigits(a)
-    if a == 0
-        return [0]
+        if a == 0
+        return [0]  # Возвращаем массив с одним элементом - 0
     end
-    B = Int[]  # Создаем пустой массив для хранения цифр
+   B = Int[]  # Создаем пустой массив для хранения цифр
     while a != 0
-        c = a % 10  # Получаем последнюю цифру числа a
+        c = mod(a, 10)  # Получаем последнюю цифру числа a с помощью оператора остатка
         push!(B, c)  # Добавляем цифру в массив
-        a = div(a, 10)  # Убираем последнюю цифру числа a
+        a = div(a, 10)  # Убираем последнюю цифру числа a с помощью целочисленного деления
     end
-    return reverse(B)  # Обратный порядок цифр в массиве для соответствия порядку в числе
-end
+    return B  # Возвращаем массив цифр
+        end
 
 function touppercase(str_)
     result = ""
@@ -25,29 +25,77 @@ function touppercase(str_)
 end
 
 function firstnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    n1=0
+    for n in vec_
+        n1 += abs(n)
+    end
+    return n1;
+    
 end
 
 function secondnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    n2=0
+    for n in vec_
+        n2 += n^2
+    end
+    return sqrt(n2);
 end
 
 function infnorm(vec_::AbstractVector{<:Number})
-    return 0;
+    n3=0
+    for n in vec_
+        if n > n3 
+            n3=n
+        end
+           return n3;
+    end
 end
 
 function firstnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
-end
+            a = zero(eltype(vec_))  # Норма  с  нулевым значением
+    
+        for n in eachcol(vec_)
+            buf = sum(n)
+            a = max(a, buf)
+        end
+    
+        return a
+    end
 
 function infnorm(vec_::AbstractMatrix{<:Number})
-    return 0;
+    a = zero(eltype(vec_))  # Норма с нулевым значением
+
+    for n in eachrow(vec_)
+        buf = sum(n)
+        a = max(a, buf)
+    end
+
+    return a
 end
 
+
 function isleap(year)
-    return false;
+    if year % 4 == 0  # Проверяем, делится ли год на 4 без остатка
+        if year % 100 == 0  # Проверяем, делится ли год на 100 без остатка
+            if year % 400 == 0  # Проверяем, делится ли год на 400 без остатка
+                return true
+            else
+                return false
+            end
+        else
+            return true
+        end
+    else
+      return false;
+end
 end
 
 function chesscolor(cell1, cell2)
-    return false;
+   # Определяем цвет первой клетки
+   color1 = (cell1[1] - 'a' + cell1[2]) % 2 == 0
+    
+   # Определяем цвет второй клетки
+   color2 = (cell2[1] - 'a' + cell2[2]) % 2 == 0
+
+   return color1 == color2
 end
